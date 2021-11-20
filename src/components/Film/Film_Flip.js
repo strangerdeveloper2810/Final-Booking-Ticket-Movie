74b974e42370c86cf31ee3a56386ac45ab84557d
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2021-11-20 11:35:41 +0700

    done project

--- src/components/Film/Film_Flip.js
+++ src/components/Film/Film_Flip.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Flim_Flip.css'
 import { history } from "../../App";
-import moment from "moment";
+
 
 
 export default function Film_Flip(props) {
@@ -9,7 +9,7 @@ export default function Film_Flip(props) {
     const { item } = props;
 
     return (
-
+        /*
         <div className="wrapper" id='phim'>
             <div className="list__cards">
                 <div className="card movie-1 card--watched">
@@ -28,8 +28,41 @@ export default function Film_Flip(props) {
                     </div>
                     <div className="card__content mt-5 text-center">
                         <h2 className='text-xl text-black '>{item.tenPhim}</h2>
-                        <h3></h3>
+                        
                     </div>
+                </div>
+            </div>
+        </div>
+        */
+        <div className="wrapper">
+            <div className="card">
+                <img src={item.hinhAnh} alt='Phim Img' />
+                <div class="badge absolute top-0 left-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Hot</div>
+                <div class="badge absolute top-0 right-0 bg-gray-700 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded text-center">
+                    {item.danhGia}
+                    <div className="rating">
+                    <i className="fa fa-star " />
+                    <i className="fa fa-star " />
+                    <i className="fa fa-star " />
+                    <i className="fa fa-star " />
+                    <i className="fa fa-star " />
+                  </div>
+                </div>
+                <div className="descriptions">
+                    <h1>{item.tenPhim}</h1>
+                    <p>
+                        {item.moTa.length > 400
+                            ? item.moTa.substr(0, 350) + "...."
+                            : item.moTa}
+                    </p>
+                    <div className="poster__footer flex flex-row relative pb-10 space-x-4 z-10">
+                        <button className="flex items-center py-2 px-4 rounded-full mx-auto text-white bg-green-600 hover:bg-green-800" onClick={() => {
+                            history.push(`detail/${item.maPhim}`);
+                        }}>
+                            Đặt Vé
+                        </button>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/components/RSlick/MultipleRowSlick.js
+++ src/components/RSlick/MultipleRowSlick.js
@@ -1,19 +1,16 @@
 import React from "react";
 import Slider from "react-slick";
-import styleSlick from "./MultipleRowSlick.module.css";
+import styleSlick from './MultipleRowSlick.module.css';
 import Film_Flip from "../Film/Film_Flip";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  SET_FILM_DANG_CHIEU,
-  SET_FILM_SAP_CHIEU,
-} from "../../redux/actions/types/QuanLyPhimType";
+import { useDispatch,useSelector } from "react-redux";
+import {SET_FILM_DANG_CHIEU,SET_FILM_SAP_CHIEU} from '../../redux/actions/types/QuanLyPhimType'
 
-function NextArrow(props) {
+function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
       className={`${className} ${styleSlick['slick-prev']}`}
-      style={{ ...style, display: "block", right: '8px' }}
+      style={{ ...style, display: "block", right: '21px' }}
       onClick={onClick}
     >
     </div>
@@ -21,7 +18,9 @@ function NextArrow(props) {
   );
 }
 
-function PrevArrow(props) {
+
+
+function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
@@ -34,86 +33,67 @@ function PrevArrow(props) {
   );
 }
 
-const settings = {
-  dots: false,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 2,
-  infinity: true,
-  arrows: true,
-  prevArrow: <PrevArrow />,
-  nextArrow: <NextArrow />,
-
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
-
 
 const MultipleRowSlick = (props) => {
   const dispatch = useDispatch();
-  const { dangChieu, sapChieu } = useSelector(
-    (state) => state.QuanLyPhimReducer
-  );
+  const {dangChieu,sapChieu} = useSelector(state => state.QuanLyPhimReducer);
 
   const renderFilms = () => {
-    return props.arrFilm.slice(0, 12).map((item, index) => {
-      return (
-        <div className="mt-3" key={index}>
-          <Film_Flip item={item} />
-        </div>
-      );
-    });
-  };
-  let activeClassDC = dangChieu === true ? "active_Film" : "none_active_Film";
 
-  let activeClassSC = sapChieu === true ? "active_Film" : "none_active_Film";
+    return props.arrFilm.slice(0, 20).map((item, index) => {
+      return <div className="mt-2" key={index}  >
+        <Film_Flip item={item} />
+      </div>
+    })
+  }
+  let activeClassDC = dangChieu===true ? 'active_Film' : 'none_active_Film';
 
-  console.log("activeSC", activeClassSC);
+  let activeClassSC = sapChieu === true ? 'active_Film' : 'none_active_Film';
+  
+  console.log('activeSC',activeClassSC)
+  
+  const settings = {
+    className: "center slider variable-width",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "20px",
+    slidesToShow: 2,
+    speed: 500,
+    rows: 2,
+    slidesPerRow: 2,
+    variableWidth: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [{
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: false,
+            centerPadding: "0x",
+            rows: 2,
+        }
+    }]
+}
+
 
 
   return (
     <div>
-      <button className={`${styleSlick[activeClassDC]} px-8 py-3 font-semibold rounded bg-gray-800 text-white mr-2`} onClick={() => {
-        const action = { type: SET_FILM_DANG_CHIEU }
-        dispatch(action);
+      <button className={`${styleSlick[activeClassDC]} px-8 py-3 font-semibold rounded bg-gray-800 text-white mr-2`} onClick={()=> {
+          const action = {type:SET_FILM_DANG_CHIEU}
+          dispatch(action);
       }}>PHIM ĐANG CHIẾU</button>
-
-      <button className={`${styleSlick[activeClassSC]} px-8 py-3 font-semibold rounded bg-white text-gray-800 border-gray-800 border`} onClick={() => {
-        const action = { type: SET_FILM_SAP_CHIEU }
+      <button className={`${styleSlick[activeClassSC]} px-8 py-3 font-semibold rounded bg-white text-gray-800 border-gray-800 border`} onClick={()=>{
+        const action = {type:SET_FILM_SAP_CHIEU}
         dispatch(action);
       }}>PHIM SẮP CHIẾU</button>
-
-
-      <Slider {...settings} >
+      <Slider {...settings}>
         {renderFilms()}
       </Slider>
-
     </div>
   );
-};
+}
+
 
 export default MultipleRowSlick;
