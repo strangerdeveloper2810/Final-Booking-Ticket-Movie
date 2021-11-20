a444d685fc0cf94d8ed161818d1447847b8cf20c
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2021-11-20 12:04:36 +0700

    done project

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -53,17 +53,13 @@ export default function Login(props) {
               />
               <g transform="matrix( 1, 0, 0, 1, 0,0) ">
                 <g>
-                  <path
-                    id="Layer0_0_1_STROKES"
-                    className="st0"
-                    d="M173.8,151.5l13.6-13.6 M35.4,89.9l29.1-29 M89.4,34.9v1 M137.4,187.9l-0.6-0.4     M36.6,138.7l0.2-0.2 M56.1,169.1l27.7-27.6 M63.8,111.5l74.3-74.4 M87.1,188.1L187.6,87.6 M110.8,114.5l57.8-57.8"
-                  />
                 </g>
               </g>
             </svg>
           </div>
           <div className="text-2xl text-indigo-800 tracking-wide ml-2 font-semibold">
-            CYBERLEARN
+        <img src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-movie-cinema-icongeek26-linear-colour-icongeek26.png" alt="icon" />
+
           </div>
         </div>
       </div>
