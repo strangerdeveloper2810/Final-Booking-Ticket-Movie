97bca576f0a58ab4f81e8d643e8401f883a31038
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-02-14 10:04:07 +0700

    fix TOKENCYBERSOFT

--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -3,5 +3,5 @@
 export const DOMAIN = `https://movienew.cybersoft.edu.vn`;
 export const TOKEN = `accessToken`;
 export const GROUPID = 'GP01';
-export const TOKENCYBERSOFT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAwOSIsIkhldEhhblN0cmluZyI6IjI3LzAxLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY0MzI0MTYwMDAwMCIsIm5iZiI6MTYxNjM0NjAwMCwiZXhwIjoxNjQzMzg5MjAwfQ.NEQRF8SKORq7R7kYbYCCO9ZZXYxTWlbaTc2wxXWMfiw"
+export const TOKENCYBERSOFT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAwOSIsIkhldEhhblN0cmluZyI6IjI0LzA1LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY1MzM1MDQwMDAwMCIsIm5iZiI6MTYxNjM0NjAwMCwiZXhwIjoxNjUzNDk4MDAwfQ.oeU8_AGwNUr93k3mqzWRv2iya3TnZarCQqdHKvpXVmM"
 export const USER_LOGIN = 'USER_LOGIN';
