b0b0b5b0c447675fff4ff1c0ea024edc69cfc324
Nguyễn Hải Trình <51492495+MainIT2810@users.noreply.github.com>
2022-05-24 16:53:19 +0700

    Update config.js

--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -3,5 +3,5 @@
 export const DOMAIN = `https://movienew.cybersoft.edu.vn`;
 export const TOKEN = `accessToken`;
 export const GROUPID = 'GP01';
-export const TOKENCYBERSOFT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAwOSIsIkhldEhhblN0cmluZyI6IjI0LzA1LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY1MzM1MDQwMDAwMCIsIm5iZiI6MTYxNjM0NjAwMCwiZXhwIjoxNjUzNDk4MDAwfQ.oeU8_AGwNUr93k3mqzWRv2iya3TnZarCQqdHKvpXVmM"
+export const TOKENCYBERSOFT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyMCIsIkhldEhhblN0cmluZyI6IjE3LzA3LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY1ODAxNjAwMDAwMCIsIm5iZiI6MTYyNjcxNDAwMCwiZXhwIjoxNjU4MTYzNjAwfQ.CyAnnc8e2Rp7YmuJCdtEj-Wp7RvlDenB9Dad6NV0R20"
 export const USER_LOGIN = 'USER_LOGIN';
