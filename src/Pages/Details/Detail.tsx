688f1390f9ad4bdafea8a90d17ea48b61d8a2581
strangerdeveloper2810 <nguyenmaster3012@gmail.com>
2023-05-05 16:51:21 +0700

    fix text filmItem.tenPhim and make ui detail page

--- src/Pages/Details/Detail.tsx
+++ src/Pages/Details/Detail.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Detail: React.FC = () => {
-  return <>Đây là trang details</>;
+  return (
+    <></>
+  );
 };
 
 export default Detail;
--- src/Pages/Film/FilmItem/FilmItem.tsx
+++ src/Pages/Film/FilmItem/FilmItem.tsx
@@ -10,17 +10,22 @@ const FilmItem: React.FC<FilmItemType> = ({ filmItem }: FilmItemType) => {
   const handleClick = () => {
     navigate(`/detail/${filmItem.maPhim}`);
   };
+  let demo: string = "Thám Tử Lừng Danh Conan: Nàng Dâu Halloween";
+  console.log("lenght", demo.length);
+
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <img
         className="rounded-t-lg"
         src={filmItem.hinhAnh}
         alt={filmItem.biDanh}
-        style={{ width: "550px", height: "550px", objectFit: "cover" }}
+        style={{ width: "500px", height: "500px", objectFit: "cover" }}
       />
 
       <div className="px-5 pb-5">
-        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mt-2">
+        <h5
+          className="filmTitle text-sm font-semibold tracking-tight text-gray-900 dark:text-white mt-2"
+        >
           {filmItem.tenPhim}
         </h5>
 
